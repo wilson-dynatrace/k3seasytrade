cc55da5387dbaff583a7ea2f34e464edcf2f3e8b
wilson-dynatrace <53265413+wilson-dynatrace@users.noreply.github.com>
2024-10-13 15:49:41 +0800

    Update post-create.sh

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -6,7 +6,8 @@ echo "post-create start"
 echo "$(date)    post-create start" >> "$HOME/status"
 
 # Start the k3s cluster
-k3d cluster create workshop --k3s-server-arg "--no-deploy=traefik" --servers 1 --agents 1 --port 8084:80@loadbalancer --wait
+k3d cluster create workshop --k3s-server-arg "--no-deploy=traefik" --servers 1 --agents 1 --wait
+#--port 8084:80@loadbalancer 
 
 # Load the sample 
 kubectl create namespace easytrade
