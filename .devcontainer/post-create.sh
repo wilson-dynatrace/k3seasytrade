b00e7ca7f9f8d272e5b95670824ece2ffc4c474d
wilson-dynatrace <53265413+wilson-dynatrace@users.noreply.github.com>
2024-10-13 15:08:08 +0800

    Update post-create.sh

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -6,13 +6,13 @@ echo "post-create start"
 echo "$(date)    post-create start" >> "$HOME/status"
 
 # Start the k3s cluster
-k3d cluster create workshop --servers 1 --agents 1
+k3d cluster create workshop --k3s-server-arg "--no-deploy=traefik" --servers 1 --agents 1 --port 8084:80@loadbalancer --wait
 
 # Load the sample 
 kubectl create namespace easytrade
 git clone https://github.com/Dynatrace/easytrade.git
-kubectl -n easytrade apply -f ./easytrade/kubernetes-manifests
-#while [[ `kubectl get pods -n easytrade | grep frontend | grep "0/"` ]]; do kubectl get pods -n easytrade; echo "==> waiting for frontend pod ready"; sleep 3; done
+#kubectl -n easytrade apply -f ./easytrade/kubernetes-manifests
+##while [[ `kubectl get pods -n easytrade | grep frontend | grep "0/"` ]]; do kubectl get pods -n easytrade; echo "==> waiting for frontend pod ready"; sleep 3; done
 
 echo "post-create complete"
 echo "$(date +'%Y-%m-%d %H:%M:%S')    post-create complete" >> "$HOME/status"
